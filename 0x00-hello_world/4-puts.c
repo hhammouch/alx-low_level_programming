1c1ac7d0d91e6e85ebd549789f156eda3c11a8bd
Hamza Hammouch <hamzapc212@gmail.com>
2024-07-14 22:21:31 -0700

    🚀

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - had l7za9 kaykteb 4i strings
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
