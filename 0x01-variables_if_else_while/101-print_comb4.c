e57e2a243dc5417afa428ab97093eaa33c1904e3
Hamza Hammouch <hamzapc212@gmail.com>
2024-07-16 18:41:06 -0700

    🌪

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+* main - comb chi t5rbi9a
+* Return: zero
+*/
+
+int main(void)
+{
+	int f, t, l;
+
+	f = '0';
+	while (f <= '7')
+	{
+		t = f + 1;
+		while (t <= '8')
+		{
+			l = t + 1;
+			while (l <= '9')
+			{
+				putchar(f);
+				putchar(t);
+				putchar(l);
+				if (f != '7' || t != '8' || l != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				l++;
+			}
+			t++;
+		}
+		f++;
+	}
+	putchar('\n');
+}
